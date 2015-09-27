7b4916f9e4ecbe1bf713b7b5a855d42b461389fa
Adrian Chlubek <luby.pl@gmail.com>
2015-09-27 19:16:14 +0200

    Added missing shaders

---
+++ binaries/media/shaders/RSMBufferData.compute.glsl
@@ -0,0 +1,47 @@
+#version 430 core
+#define NO_FS
+vec3 CameraPosition;
+
+#include UsefulIncludes.glsl
+#define MATH_E 2.7182818284
+float reverseLogEx(float dd, float far){
+	return pow(2, dd * log2(far+1.0)) - 1;
+}
+layout(binding = 22) uniform usampler2D LightData;
+layout(binding = 23) uniform sampler2D LightDepth;
+
+layout( local_size_x = 1, local_size_y = 1, local_size_z = 1 ) in;
+uniform mat4 MatP;
+uniform mat4 MatV;
+uniform mat4 MatI;
+uniform float Far;
+uniform vec3 LightPos;
+void main(){
+
+    float ax = float(gl_WorkGroupID.x) / 32.0;
+    float ay = float(gl_WorkGroupID.y) / 32.0;
+    vec2 UV = vec2(ax, ay);
+    ivec2 iUV = ivec2(vec2(ax, ay) * vec2(textureSize(LightData, 0)));
+    uint gIndex = 
+        //gl_GlobalInvocationID.z * gl_GlobalInvocationID.x * gl_GlobalInvocationID.y +
+        gl_WorkGroupID.y * gl_WorkGroupID.x + 
+        gl_WorkGroupID.x;
+    float ldep = texelFetch(LightDepth, iUV, 0).r;            
+    uvec2 lcolord =  texelFetch(LightData, iUV, 0).rg;
+    vec4 upackA = unpackUnorm4x8(lcolord.r);
+    vec4 upackB = unpackSnorm4x8(lcolord.g);
+    vec3 lcolor = upackA.rgb;
+    float lrough = upackA.a;
+    vec3 lnormal = upackB.rgb;
+    float lmetal = upackB.a;
+    
+    UV = (UV * 2 - 1);
+    vec4 reconstructDir = inverse(MatP * MatV * MatI) * vec4(UV, 1.0, 1.0);
+    reconstructDir.xyz /= reconstructDir.w;
+    
+    float revlog = reverseLogEx(ldep, Far);
+    // not optimizable
+    vec3 newpos = normalize(reconstructDir.xyz - LightPos) * revlog + LightPos;
+    RSMLight light = RSMLight(vec4(newpos, lrough), vec4(lnormal, lmetal), vec4(lcolor, 1));
+    rsmLights[gIndex] = light;
+}
---
+++ binaries/media/shaders/include/Shade.glsl
@@ -0,0 +1,148 @@
+
+#define PI 3.14159265
+
+float orenNayarDiffuse(
+vec3 lightDirection,
+vec3 viewDirection,
+vec3 surfacenormal,
+float roughness,
+float albedo) {
+
+    float LdotV = dot(lightDirection, viewDirection);
+    float NdotL = dot(lightDirection, surfacenormal);
+    float NdotV = dot(surfacenormal, viewDirection);
+
+    float s = LdotV - NdotL * NdotV;
+    float t = mix(1.0, max(NdotL, NdotV), step(0.0, s));
+
+    float sigma2 = roughness * roughness;
+    float A = 1.0 + sigma2 * (albedo / (sigma2 + 0.13) + 0.5 / (sigma2 + 0.33));
+    float B = 0.45 * sigma2 / (sigma2 + 0.09);
+
+    return albedo * max(0.0, NdotL) * (A + B * s / t) / PI;
+}
+
+float beckmannDistribution(float x, float roughness) {
+    float NdotH = max(x, 0.001);
+    float cos2Alpha = NdotH * NdotH;
+    float tan2Alpha = (cos2Alpha - 1.0) / cos2Alpha;
+    float roughness2 = roughness * roughness;
+    float denom = 3.141592653589793 * roughness2 * cos2Alpha * cos2Alpha;
+    return exp(tan2Alpha / roughness2) / denom;
+}
+
+float beckmannSpecular(
+vec3 lightDirection,
+vec3 viewDirection,
+vec3 surfacenormal,
+float roughness) {
+    return beckmannDistribution(dot(surfacenormal, normalize(lightDirection + viewDirection)), roughness);
+}
+
+float cookTorranceSpecular(
+vec3 lightDirection,
+vec3 viewDirection,
+vec3 surfacenormal,
+float roughness,
+float fresnel) {
+
+    float VdotN = max(dot(viewDirection, surfacenormal), 0.0);
+    float LdotN = max(dot(lightDirection, surfacenormal), 0.0);
+
+    //Half angle vector
+    vec3 H = normalize(lightDirection + viewDirection);
+
+    //Geometric term
+    float NdotH = max(abs(dot(surfacenormal, H)), 0.0);
+    float VdotH = max(abs(dot(viewDirection, H)), 0.0001);
+    float LdotH = max(abs(dot(lightDirection, H)), 0.0001);
+    float G1 = (2.0 * NdotH * VdotN) / VdotH;
+    float G2 = (2.0 * NdotH * LdotN) / LdotH;
+    float G = min(1.0, min(G1, G2));
+
+    //Distribution term
+    float D = beckmannDistribution(NdotH, roughness);
+
+    //Fresnel term
+    float F = 1;
+
+    //Multiply terms and done
+    return  G * F * D / max(3.14159265 * VdotN, 0.001);
+}
+
+float CalculateFallof( float dist){
+    //return 1.0 / pow(((dist) + 1.0), 2.0);
+    return dist == 0 ? 1 : (1) / (PI*dist*dist+1);
+    
+}
+
+vec3 shade(
+    vec3 albedo, 
+    vec3 normal,
+    vec3 fragmentPosition, 
+    vec3 lightPosition, 
+    vec4 lightColor, 
+    float roughness, 
+    float metalness, 
+    float specular,
+    bool ignoreAtt
+){
+    vec3 lightRelativeToVPos =normalize( lightPosition - fragmentPosition);
+    
+    vec3 cameraRelativeToVPos = normalize(-ToCameraSpace(fragmentPosition));
+    
+    float distanceToLight = distance(fragmentPosition, lightPosition);
+    float att = ignoreAtt ? 1 : (CalculateFallof(distanceToLight)* lightColor.a);
+   // if(att < 0.002) return vec3(0);
+    
+    float specularComponent = specular * clamp(cookTorranceSpecular(
+        lightRelativeToVPos,
+        cameraRelativeToVPos,
+        normal,
+        max(0.02, roughness), 1
+        ), 0.0, 1.0);
+
+    
+    float diffuseComponent = clamp(orenNayarDiffuse(
+        lightRelativeToVPos,
+        cameraRelativeToVPos,
+        normal,
+        max(0.02, roughness), 1
+        ), 0.0, 1.0);   
+
+    vec3 cc = mix(lightColor.rgb*albedo, lightColor.rgb, metalness);
+    
+    vec3 difcolor = cc * diffuseComponent * att;
+    vec3 difcolor2 = lightColor.rgb * albedo * diffuseComponent * att;
+    vec3 specolor = cc * specularComponent;
+    
+    return mix(difcolor2 + specolor, difcolor*(roughness) + specolor, metalness);
+}
+
+vec3 shadeUV(vec2 uv,
+    vec3 lightPosition, 
+    vec4 lightColor
+){
+    vec3 position = FromCameraSpace(texture(worldPosTex, UV).rgb);
+    vec3 albedo = texture(diffuseColorTex, UV).rgb;
+    float specular = texture(worldPosTex, UV).a;
+    vec3 normal = normalize(texture(normalsTex, UV).rgb);
+      
+    float roughness = texture(meshDataTex, UV).a;
+    float metalness =  texture(meshDataTex, UV).z;
+    return shade(albedo, normal, position, lightPosition, lightColor, roughness, metalness, specular, false);
+}
+
+vec3 shadeUVNoAtt(vec2 uv,
+    vec3 lightPosition, 
+    vec4 lightColor
+){
+    vec3 position = FromCameraSpace(texture(worldPosTex, UV).rgb);
+    vec3 albedo = texture(diffuseColorTex, UV).rgb;
+    float specular = texture(worldPosTex, UV).a;
+    vec3 normal = normalize(texture(normalsTex, UV).rgb);
+      
+    float roughness = texture(meshDataTex, UV).a;
+    float metalness =  texture(meshDataTex, UV).z;
+    return shade(albedo, normal, position, lightPosition, lightColor, roughness, metalness, specular, true);
+}
